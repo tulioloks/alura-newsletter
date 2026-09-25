ebb4c76b28f3b6ca761e74adc60978d912e35911
tulioloks <tulioloks@hotmail.com>
2026-09-25 11:07:50 -0300

    Finalizado o projeto

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
+import { useState } from "react";
+import ArticleList from "./components/ArticleList/ArticleList";
 import Header from "./components/Header/Header";
+import Form from "./components/Form/Form";
 
 const App = () => {
-  return <Header />
+  const [user, setUser] = useState();
+  const hasUser = Boolean(user);
+  return (
+  <div className="h-screen">
+     <Header user={user} />
+      { hasUser && <ArticleList /> }
+      { hasUser || <Form onSubmit={setUser} /> }
+  </div>
+  )
 }
 
 
